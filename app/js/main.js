e533dbc88889c3859d0002bd9333a17e66b6a166
Dmitry <morkvart@gmail.com>
2023-04-22 12:12:25 +0300

    add cases section, add contact in header

--- app/js/main.js
+++ app/js/main.js
@@ -9,3 +9,27 @@ for (let i = 0; i < menuLinks.length; i++) {
         }
     })
 }
+
+
+// swiper
+const swiper = new Swiper('.cases__swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.cases__swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.cases__swiper-btn-next',
+        prevEl: '.cases__swiper-btn-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
